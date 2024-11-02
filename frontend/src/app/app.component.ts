ba906ed8e4d740d88e5037d5550286d4ebc1105c
Samuel-Brr <samuel.barre972@gmail.com>
2024-11-02 16:58:14 +0100

    Ajout d'un début de page d'acceuil et des routes pour la connexion et l'inscription

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatToolbar, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/features/utilisateur/utilisateur-routing.module.ts
+++ frontend/src/app/features/utilisateur/utilisateur-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InscriptionComponent} from "./components/inscription/inscription.component";
+import {ConnexionComponent} from "./components/connexion/connexion.component";
+import {ProfilComponent} from "./components/profil/profil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { title: 'Register', path: 'register', component: InscriptionComponent },
+  { title: 'Login', path: 'login', component: ConnexionComponent },
+  { title: 'Profil', path: 'profil', component: ProfilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
