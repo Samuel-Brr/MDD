a48f99a103fbfee790fbe46fb42d0dd6964d156c
Samuel-Brr <samuel.barre972@gmail.com>
2024-11-10 17:27:17 +0100

    Ajout de l'html css pour l'écran de connexion ainsi que le ts du formulaire

---
+++ frontend/src/app/features/utilisateur/components/inscription/inscription.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../../../shared/components/header/header.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-inscription',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    RouterLink
+  ],
+  templateUrl: './inscription.component.html',
+  styleUrl: './inscription.component.css'
+})
+export class InscriptionComponent {
+  signupForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      // Implement your signup logic here
+    }
+  }
+}
