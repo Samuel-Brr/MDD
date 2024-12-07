323a30577ee1bfd61a1ebf0b93c25be6bb7395bd
Samuel-Brr <samuel.barre972@gmail.com>
2024-12-07 16:37:33 +0100

    Refacto de la couleur des boutons

--- frontend/src/app/features/article/components/creer-article/creer-article.component.ts
+++ frontend/src/app/features/article/components/creer-article/creer-article.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
@@ -9,26 +9,34 @@ import {MatIconModule} from "@angular/material/icon";
 import {ArticleService} from "../../services/article.service";
 import {Article} from "../../../../shared/interfaces/article.interface";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {ThemeService} from "../../../theme/services/theme.service";
+import {Theme} from "../../../../shared/interfaces/theme";
 
 @Component({
   selector: 'app-creer-article',
   standalone: true,
-    imports: [
-      ReactiveFormsModule,
-      MatCardModule,
-      MatInputModule,
-      MatButtonModule,
-      MatIconModule,
-      MatFormFieldModule,
-      RouterLink
-    ],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    RouterLink,
+    MatSelect,
+    MatOption
+  ],
   templateUrl: './creer-article.component.html',
   styleUrl: './creer-article.component.css'
 })
-export class CreerArticleComponent {
+export class CreerArticleComponent implements OnInit {
+
   fb = inject(FormBuilder);
   articleService = inject(ArticleService);
+  themeService = inject(ThemeService);
   snackBar = inject(MatSnackBar);
+  themes: Theme[] = [];
 
   articleForm: FormGroup = this.fb.group({
     theme: ['', [Validators.required]],
@@ -36,6 +44,17 @@ export class CreerArticleComponent {
     contenu: ['', [Validators.required]]
   });
 
+
+  ngOnInit(): void {
+    this.themeService.getThemes().subscribe({
+      next: (themes) => {
+        this.themes = themes.themes;
+      },
+      error: () => {
+        this.snackBar.open('Erreur lors de la récupération des thèmes', 'Fermer', {duration: 3000});
+      }
+    });
+  }
 
   onSubmit() {
     if (this.articleForm.valid) {
